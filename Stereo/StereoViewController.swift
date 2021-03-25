f11ce6816240a91a11ffc448622a5078ffead528
Invader Zim <pwstern@gmail.com>
2021-03-25 13:55:24 -0700

    Created simulation to rotate square 360 degrees in 4 seconds.

--- Stereo/StereoViewController.swift
+++ Stereo/StereoViewController.swift
@@ -7,10 +7,33 @@
 
 import UIKit
 
+struct Constants {
+    static let frameTime = 0.02  // seconds
+    static let rotationPeriod = 4.0  // seconds per 360 degree rotation
+}
+
 class StereoViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var rotation = 0.0
+    
+    private var simulationTimer = Timer()
+    
+    @IBOutlet weak var stereoView: StereoView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        startSimulation()
+    }
+
+    private func startSimulation() {
+        simulationTimer = Timer.scheduledTimer(timeInterval: Constants.frameTime, target: self,
+                                               selector: #selector(updateSimulation),
+                                               userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateSimulation() {
+        let deltaAngle = Constants.frameTime / Constants.rotationPeriod * 360  // degrees
+        rotation = (rotation + deltaAngle).truncatingRemainder(dividingBy: 360)
+        stereoView.rotation = CGFloat(rotation)
     }
 }
